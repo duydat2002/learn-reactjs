570c59a3d60f4aeb09e722d066a1b1522d0bd5e3
Duy Dat <duydat30122002@gmail.com>
2021-10-03 23:08:35 +0700

    Router

--- src/App.js
+++ src/App.js
@@ -1,11 +1,40 @@
 import React from 'react';
+import { Link, NavLink, Route } from 'react-router-dom';
 // import './styles.scss';
-import CustomHook from './features/HookBasic/index';
+import TodoFeature from './features/Todo/index';
+import AlbumFeature from './features/Album/index';
+import StateBasic from './features/StateBasic/index';
+import ColorBox from './features/HookBasic/index';
 
 const App = () => {
     return (
         <div className="App">
-            <CustomHook/>
+            Header
+            <Link to="/todo">
+                <p>Todo</p>
+            </Link>
+            <Link to="/album">
+                <p>Album</p>
+            </Link>
+            <Link to="/state">
+                <p>State Basic</p>
+            </Link>
+            <Link to="/hook">
+                <p>Custom hook</p>
+            </Link>
+
+            <p>Khi bấm sẽ có thêm class active</p>
+            <NavLink to="/todo"><p>Todo</p></NavLink>
+            <NavLink to="/album"><p>Album</p></NavLink>
+            <NavLink to="/state"><p>State Basic</p></NavLink>
+            <NavLink to="/hook"><p>Custom hook</p></NavLink>
+
+            <Route path="/todo" component={TodoFeature}/>
+            <Route path="/album" component={AlbumFeature}/>
+            <Route path="/state" component={StateBasic}/>
+            <Route path="/hook" component={ColorBox}/>
+            <br/>
+            Footer
         </div>
     );
 };
